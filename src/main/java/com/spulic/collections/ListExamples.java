d88e17d18783190c584a0079d0e38fd7aa51e210
spulic <satishpulickal@gmail.com>
2017-07-11 22:36:48 -0500

    comments + ordered

--- src/main/java/com/spulic/collections/ListExamples.java
+++ src/main/java/com/spulic/collections/ListExamples.java
@@ -14,44 +14,10 @@ public class ListExamples {
 		//PriorityQueue implemented Queue
 		priorityQueue();
 		//StackQueue
-		stackList();
+		stack();
 		
 	}
 	
-	public static void stackList() {
-		List<String> stackList = new Stack<String>();
-		stackList.add("first");
-		stackList.add("second");
-		stackList.add("thrid");
-		stackList.add("fourth");
-		
-		System.out.println(System.lineSeparator() + "StackList values..");
-		for(String s: stackList) {
-			print(s);
-		}
-		stackList.remove(0);
-		System.out.println(System.lineSeparator() + "StackList values after the 0th element is removed");
-		for(String s: stackList) {
-			print(s);
-		}
-		
-	}
-
-	public static void priorityQueue() {
-		Queue<String> qPriority = new PriorityQueue<String>();
-		qPriority.add("q1");
-		qPriority.add("q4");
-		qPriority.add("q3");
-		qPriority.add("q2");
-				
-		//print the priority Queue
-		System.out.println(System.lineSeparator() + "PriorityQueue values..");
-		System.out.println("[q1, q4, q3, q2]");
-		for (String s : qPriority) {
-			print(s);
-		}
-	}
-
 	public static void linkedListQueue() {
 		//Queue hiearchy and it's class, implementing & extending interfaces
 		Queue<String> qList = new LinkedList<String>();
@@ -61,9 +27,59 @@ public class ListExamples {
 		qList.add("q3");
 		
 		//print the LinkedList Queue
-		System.out.println("LinkedListQueue values..");
-		System.out.println(qList);
+		System.out.println("** LinkedListQueue values (FIFO) **");
+		System.out.println("In the orders of addition => " + qList);
+		System.out.print("In the order of storage => ");
 		for(String s : qList) {
+			print(s);
+		}
+		
+	}
+	
+	public static void priorityQueue() {
+		System.out.println(System.lineSeparator());
+		PriorityQueue<String> priorityQ = new PriorityQueue<String>();
+		priorityQ.add("q1");
+		priorityQ.add("q4");
+		priorityQ.add("q3");
+		priorityQ.add("q2");
+				
+		//print the priority Queue
+		System.out.println(System.lineSeparator() + "** PriorityQueue values, Ordered by priorty, Out of Queue depends on priority **");
+		System.out.println("In the orders of addition => [q1, q4, q3, q2]");
+		System.out.print("In the order of storage => ");
+		for (String s : priorityQ) {
+			print(s);
+		}
+		String value = priorityQ.peek();
+		System.out.println("\nValue of the element in the queue (peek value) => " + value);
+		//poll out the element
+		priorityQ.poll();
+		//print the queue
+		System.out.print("Value of the element in the queue after the poll => ");
+		for (String s : priorityQ) {
+			print(s);
+		}
+	}
+
+	public static void stack() {
+		System.out.println(System.lineSeparator());
+		Stack<String> stack = new Stack<String>();
+		stack.push("first");
+		stack.push("second");
+		stack.push("third");
+		stack.push("fourth");
+		
+		
+		System.out.println(System.lineSeparator() + "** Stack Orders (LIFO) **");
+		System.out.print("In the orders of addition => ");
+		for(String s: stack) {
+			print(s);
+		}
+		//pop out the last element added to the stack
+		stack.pop();
+		System.out.print(System.lineSeparator() + "Stack values after an element is poped out => ");
+		for(String s: stack) {
 			print(s);
 		}
 		
